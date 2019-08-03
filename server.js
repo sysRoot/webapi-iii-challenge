dd211cedbd3fb1a876bd0258d0cbaa4313c07704
Jason Cook <the.root.user@gmail.com>
2019-08-03 14:15:49 -0400

    Remove package-lock.json, start basic server

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const express = 'express';
+const express = require('express');
+
+const postRouter = require('./posts/postRouter')
 
 const server = express();
+
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.path}`);
+
+  next();
+};
+
+server.use(logger);
+server.use(express.json());
+
+server.use('/api/posts', postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
-
-function logger(req, res, next) {
-
-};
 
 module.exports = server;
